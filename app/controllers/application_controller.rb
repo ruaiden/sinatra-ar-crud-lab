ffabdf7e392c50229f40206c3052bf14b1b51af2
Aiden <aiden.marques94@gmail.com>
2021-02-17 20:58:50 -0500

    commit tree c445ae4deecc428ce35ae74960d4428890589401

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,47 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "./articles"
   end
+
+  get '/articles' do 
+    @articles = Article.all
+    erb :index
+  end 
+
+  get '/articles/new' do 
+    @article = Article.new
+    erb :new
+  end 
+
+  post "/articles" do
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  # edit
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  # update
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  #destroy
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
+
+  
 end
